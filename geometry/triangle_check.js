5a975b00a3c9f897750b751805e934d25c5d85c5
Qasim Dove <emailqasim@gmail.com>
2015-02-12 20:20:18 -0500

    new file:   geometry/pythagorean_theorem.js 	new file:   geometry/triangle_check.js

---
+++ geometry/triangle_check.js
@@ -0,0 +1,11 @@
+triangleCheck = function(a, b, c){
+    var result;
+    
+    result = a + b;
+    
+    if(result > c){
+        return ("This is a triangle.");
+    }else{
+        return ("This is not a triangle.");
+    }
+}
